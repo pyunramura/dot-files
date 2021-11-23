10b06b72b6035fbce2c964e939d41ab014d1fcd7
pyunramura <jeremy.cummings@live.com>
2021-11-23 06:26:19 -0600

    simplified stow statement

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 STOW_APPS="$*"
+TARGET=~/
 
 DIR=$(echo $0 | rev | cut -d '/' -f2- | rev)
 cd $DIR
 
-OVERRIDE="bash"
-TARGET=~/
-for k in $OVERRIDE; do
+for k in $STOW_APPS; do
 for i in $(find ./$k -type f | cut -d '/' -f3-) ; do
 rm -f $TARGET$i
 done
